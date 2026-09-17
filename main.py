5676c9c66f44560a1a3a4811171960778bcf517f
Adnanadip <adnanadip900@gmail.com>
2026-09-17 22:33:06 +0600

    Adjust game dimensions and scale for improved visual layout

--- main.py
+++ main.py
@@ -9,8 +9,8 @@ from effects import ParticleSystem
 pygame.init()
 pygame.mixer.init()
 
-GAME_W, GAME_H = 500, 400
-SCALE = 3
+GAME_W, GAME_H = 500, 375
+SCALE = 2
 WINDOW_W, WINDOW_H = GAME_W * SCALE, GAME_H * SCALE
 
 SCREEN = pygame.display.set_mode((WINDOW_W, WINDOW_H))
